df3c12206ea13a191f34f0daedae12972a31a4c2
eljjo <elbieta.dalik@gmail.com>
2026-02-18 23:35:26 +0100

    add missing has_mamy

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,5 @@
 
 class Group < ApplicationRecord
   belongs_to :owner, class_name: 'User'
+  has_many :memberships
 end
--- db/migrate/20260218181339_create_group.rb
+++ db/migrate/20260218181339_create_group.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateGroup < ActiveRecord::Migration[8.1]
   def change
     create_table :groups do |t|
